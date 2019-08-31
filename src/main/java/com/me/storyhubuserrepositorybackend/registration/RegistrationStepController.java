27d86997bad2d630dc7ec60d62819e556ad84ea6
Damian Sulewski <damiansulewskiwork@gmail.com>
2019-08-31 16:07:58 +0200

    Change registration step controller url

--- src/main/java/com/me/storyhubuserrepositorybackend/registration/RegistrationStepController.java
+++ src/main/java/com/me/storyhubuserrepositorybackend/registration/RegistrationStepController.java
@@ -3,6 +3,7 @@ package com.me.storyhubuserrepositorybackend.registration;
 import com.me.storyhubuserrepositorybackend.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,12 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("registration/step")
+@RequestMapping("registration")
 public class RegistrationStepController {
     private final UserService userService;
 
     @PostMapping("create-user")
-    public void createUser(@Valid CreateUserRequest request) {
+    public void createUser(@Valid @RequestBody CreateUserRequest request) {
         userService.createUser(request);
     }
 }
